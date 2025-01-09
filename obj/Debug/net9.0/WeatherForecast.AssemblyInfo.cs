34f1769bc18cb4f8e2a215f2e4b02154494837ce
PAWAN SINGH NEGI <sky@PAWANs-MacBook-Air.local>
2025-01-09 12:50:32 +0530

    Changed to index.html

--- obj/Debug/net9.0/WeatherForecast.AssemblyInfo.cs
+++ obj/Debug/net9.0/WeatherForecast.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherForecast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe61f53ec63316f4d06e1604be6634bf75f1b79a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherForecast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherForecast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
